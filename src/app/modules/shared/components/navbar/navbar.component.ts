bf0ec0e8249b8272583bc3019b8cda90618ccccb
thiagofrede <thiagofredes1@gmail.com>
2024-08-30 10:55:44 -0300

    creamos el cambiar de fondo a negro

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+//funcion para modo oscuro
+cambiarFondo(){
+  let toggle: HTMLInputElement | null = document.getElementById('toggle') as HTMLInputElement
+  let label_toggle: HTMLElement | null = document.getElementById('label_toggle') as HTMLElement
+  if(toggle){
+    let checked: boolean = toggle.checked;
+    document.body.classList.toggle('dark', checked)
+    
+    if(checked){
+      label_toggle!.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    }else{
+      label_toggle.innerHTML = '<i class="fa-solid fa-moon"></i>'
+    }
 
+
+  }
+}
 }
